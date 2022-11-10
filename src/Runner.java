eaa190fc6b710bb82e279bef3ad5194094481e3c
Alexandre Arturi <alexandrea8@nycstudents.net>
2022-11-10 12:20:15 -0500

    11/10

--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Runner {
+public class Runner {
+    public static void main (String []args){
+        StringLoops loops = new StringLoops();
+        System.out.println(loops.countCharacters("a", "Apple and banana"));
+        System.out.println(loops.countCharacters("A", "Apple and banana"));
+        System.out.println(loops.countCharacters("!", "Hello! Nice day!"));
+        System.out.println(loops.reverseString("hello!"));
+        System.out.println(loops.reverseString("Apples and bananas"));
+
+
+    }
 }
--- src/StringLoops.java
+++ src/StringLoops.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class StringLoops {
+public class StringLoops {
+    public StringLoops() { }
+
+    public int countCharacters(String character, String searchString) {
+        String slow = searchString.toLowerCase();
+        String clow = character.toLowerCase();
+        int count = 0;
+
+        for (int i = 0; i<slow.length(); i++){
+            String c = slow.substring(i, i+1);
+            if (c.equals(clow)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public String reverseString(String origString) {
+        String newString = "";
+        for (int i = origString.length()-1; i>=0; i--){
+            String ch = origString.substring(i, i + 1);
+            newString +=ch;
+        }
+        return newString;
+    }
 }
